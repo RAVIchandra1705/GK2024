65eb6260c9e06b6db2b15404a6f5eb5a1efc7182
Ravindra K <RAVIchandra1705>
2024-09-05 19:10:12 +0530

    initial commit

---
+++ GK2024/script.js
@@ -0,0 +1,310 @@
+const data = [
+  {
+    id: '1',
+    question:"When was India got Independence?",
+    answer:
+      "15th August, 1947"
+  },
+  {
+    id: '2',
+    question:"Who is the head of the executive branch of the Government of India?",
+    answer: "The Prime Minester "
+  },
+  {
+    id: '3',
+    question:"Who is the present Prime Minister of India?",
+    answer: "Shri Narendra Modi \n -Shri Narendra Modi was sworn-in as India’s Prime Minister for the third time on 9th June 2024, following another decisive victory in the 2024 Parliamentary elections. This victory marked the third consecutive term for Shri Narendra Modi, further solidifying his leadership."
+  },
+
+  {
+    id: '4',
+    question: "How many memners are there in the Rajya Sabha?",
+    answer: "245 members"
+  },
+
+  {
+    id: '5',
+    question:"What is the minimum age for becoming the President of India?",
+    answer: "35 Years"
+  },
+
+  {
+    id: '6',
+    question: "Who is the present President of India?",
+    answer: "Smt Droupadi Murmu\n -The President of India  is the head of state of the Republic of India. The president is the nominal head of the executive, the first citizen of the country, as well as the supreme commander of the Indian Armed Forces. Droupadi Murmu is the 15th and current president, having taken office from 25 July 2022."
+
+  },
+  {
+    id: '7',
+    question: "What is the tenure of the Lok Sabha?",
+    answer: "5 years"
+  },
+  {
+    id: '8',
+    question: "Which body in India is responsible for conducting elections?",
+    answer: "The Election Commission of India"
+  },
+  {
+    id: '9',
+    question: "Which amendment added the words 'Socialist' and 'Secular' to the Preamble of the Indian Constitution?",
+    answer: "The 42nd Amendment"
+  },
+  {
+    id: '10',
+    question: "Which part of the Indian Constitution deals with Fundamental Rights?",
+    answer: "Part III"
+  },
+
+  {
+    id: '11',
+    question: "Under which article can the President of India declare a national emergency?",
+    answer:"Article 352"
+  },
+  {
+    id:'12',
+    question:"How many schedules are there in the Indian Constitution?",
+    answer: "12 schedules."
+  },
+  {
+    id: '13',
+    question:"Who is the presiding officer of the Lok Sabha? A: The Speaker of the Lok Sabha?",
+    answer:"The Speaker of Lok Shabha"
+  },
+  {
+    id: '14',
+    question: "Who is the current Lok Shabha speaker?",
+    answer: "Om Birla<br> The speaker of the Lok Sabha is the presiding officer and the highest authority of the Lok Sabha, the lower house of the Parliament of India. The speaker is elected generally in the first meeting of the Lok Sabha following general elections. The speaker does not enjoy a security of tenure and his term is subjected to the pleasure of the house i.e. can be removed anytime by a resolution of the Lok Sabha by a majority of the all the then members of the house. The longest-serving speaker was Balram Jakhar, whose tenure lasted 9 years and 329 days."
+  },
+
+  {
+    id: '15',
+    question: "Which body has the power to review and interpret the Constitution of India?",
+    answer:"The Supreme Court of India"
+  },
+  {
+    id: '16',
+    question: "What is the longest river in India?",
+    answer: "The Ganga(Ganges)\n -The Ganga is India's most important and iconic river. It flows down from its glacial source in the high Himalayas to course through five states in the northern plains before draining into the swirling waters of the Bay of Bengal through the Sunderbans delta, the largest mangrove system in the world."
+  },
+  {
+    id: '17',
+    question: "Which is the largest state in the India",
+    answer:"Rajasthan"
+  },
+  {
+    id: '18',
+    question: "Which mountain range forms the northen border of India?",
+    answer:"Himalayas"
+  },
+  {
+    id: '19',
+    question:"Which state in India is known as the 'Land of Five Rivers'?",
+    answer: "Punjab"
+  },
+  {
+    id: '20',
+    question: "What is the southernmost point of India?",
+    answer: "Indira Point (Nicobar Islands)"
+  },
+  {
+    id: '21',
+    question: "Which Indian state has the longest coastline?",
+    answer: "Gujarat"
+  },
+
+  {
+    id: '22',
+    question: "What is the name of the highest peak in India?",
+    answer: " Kangchenjunga."
+  },
+
+  {
+    id: '23',
+    question: "Which river is known as the 'Sorrow of Bihar' due to its frequent flooding?",
+    answer: "Kosi River"
+  },
+  { 
+    id: '24',
+    question: "In which state is the Great Indian Desert (Thar Desert) located?",
+    answer: "Rajasthan"
+  },
+  {
+    id: '25',
+    question: "Which Indian state is the leading producer of tea?",
+    answer: "Assam"
+  },
+  {
+
+    id: '26',
+    question:"What is the major natural port located in Tamil Nadu?",
+    answer: "Chennai Port"
+  },
+  {
+
+    id: '27',
+    question:"The Tropic of Cancer passes through how many Indian states?",
+    answer: "Eight states"
+  },
+  {
+    id: '28',
+    question: "Which Indian state has the largest forest cover?",
+    answer: "Madhya Pradesh"
+  },
+  {
+    id: '29',
+    question: "What is the largest freshwater lake in India?",
+    answer: "Wular Lake (Jammu & Kashmir)"
+  },
+  {
+    id: '30',
+    question: "Which plateau is known as the 'Roof of South India'?",
+    answer: "The Deccan Plateau"
+  },
+  {
+    id: '31',
+    question: "Which river forms the boundary between Haryana and Uttar Pradesh?",
+    answer: "Yamuna River"
+  },
+  {
+    id: '32',
+    question: "Which state in India receives the highest rainfall?",
+    answer: "Meghalaya"
+  },
+  {
+    id: '33',
+    question: "In which state is the Sundarbans Delta, the largest mangrove forest in the world, located?",
+    answer: " West Bengal"
+  },
+  {
+    id: '34',
+    question: "What is the name of the highest waterfall in India?",
+    answer: "Kunchikal Falls (Karnataka)"
+  },
+  {
+    id: '35',
+    question: " What is the aim of the Pradhan Mantri Jan Dhan Yojana (PMJDY)?",
+    answer: "Financial inclusion by ensuring access to banking services for all households."
+  },
+  {
+    id: '36',
+    question: "What is the purpose of the Swachh Bharat Mission?",
+    answer: "To achieve universal sanitation coverage and cleanliness across India"
+  },
+  {
+    id: '37',
+    question: "What does the PM-KISAN scheme provide to farmers?",
+    answer: "Direct income support of ₹6,000 per year to farmers"
+  },
+  {
+    id: '38',
+    question: "What is the objective of the Make in India initiative?",
+    answer: "To encourage companies to manufacture products in India and boost the nation's economy"
+  },
+  {
+    id: '39',
+    question: "What does the Ayushman Bharat Yojana provide?",
+    answer: "Health insurance coverage of ₹5 lakhs per family per year for secondary and tertiary care hospitalization"
+  },
+  {
+    id: '40',
+    question: "What is the goal of the Digital India initiative?",
+    answer: "To transform India into a digitally empowered society and knowledge economy",
+  },
+  {
+    id: '41',
+    question: "What does the Beti Bachao Beti Padhao scheme aim to address?",
+    answer: " Gender discrimination and promote education for girl children"
+  },
+  {
+    id: '42',
+    question: "What is the Smart Cities Mission focused on?",
+    answer:" Developing smart cities with sustainable and efficient infrastructure and services"
+  },
+  {
+    id: '43',
+    question: "What is the purpose of the Atal Pension Yojana?",
+    answer: "To provide social security to workers in the unorganized sector by offering pension benefits"
+  },
+  {
+    id: '44',
+    question: "What does the Ujjwala Yojana aim to achieve?",
+    answer: "Providing free LPG connections to women from below-poverty-line (BPL) households"
+  },
+  {
+    id: '45',
+    question:" Who is the current Financial Minister of India?",
+    answer: "Nirmala Seetha Raman"
+  },
+  {
+    id: '46',
+    question: "Who is the present Vice-president of India?",
+    answer:"Shri Jagdeep Dhankar<br> "
+  },
+  {
+    id: '47',
+    question: "Which country have large border to India?",
+    answer: "Bangladesh"
+  },
+  {
+    id: '48',
+    question: "What is the national Animal and Bird of India",
+    answer: "ANIMAL: 'Bengal Tiger' and BIRD: 'Peacock'"
+  },
+  {
+    id: '49',
+    question: "Where is the Taj Mahal Located?",
+    answer: "Agra, India"
+  },
+  {
+    id: '50',
+    question: "Who is the chairman of ISRO(Indian Space Research Organisation)?",
+    answer: "Sreedhara Panicker Somanath"
+  },
+
+
+];
+
+const GK2024Wrapper = document.querySelector(".gkIndia");
+
+//create a function to validate data with questions and answers
+function createGKIndiaData(){
+  GK2024Wrapper.innerHTML = data.map(
+    (dataItem) =>`
+    <div class="gkIndia_item">
+    <div class="gkIndia_title">
+    <h3>${dataItem.question}</h3>
+    <i class="fa-solid fa-arrow-down"></i>
+    </div>
+    <div class = "gkIndia_content">
+    <p>${dataItem.answer}</p>
+    </div>
+    </div>`).join(" ");
+}
+
+createGKIndiaData();
+const getGKIndiaTitles = document.querySelectorAll(".gkIndia_title");
+
+console.log('====================================================');
+console.log(getGKIndiaTitles);
+console.log('====================================================');
+
+getGKIndiaTitles.forEach((currentItem) => {
+  currentItem.addEventListener("click", (event) =>{
+    if(currentItem.classList.contains("active")){
+      currentItem.classList.remove("active");
+    }
+    else{
+      let getAlreadyAddedActiveClasses = document.querySelectorAll(".active");
+      getAlreadyAddedActiveClasses.forEach((currentActiveItem) => {
+        currentActiveItem.classList.remove("active");
+      });
+      currentItem.classList.add("active");
+    }
+  });
+  
+});
+
+
+
+
+  
